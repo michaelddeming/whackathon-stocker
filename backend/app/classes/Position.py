64bd1ef24b342daeab7a2daadf7a2bfec724ac32
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-06-29 18:54:14 -0700

    Add Position functionality.

--- backend/app/classes/Position.py
+++ backend/app/classes/Position.py
@@ -7,19 +7,81 @@ class Position:
     def __init__(self, 
                  ticker: str,
                  shares: float,
-                 average_cost: float,
-                 current_price: float,
-                 total_value: float,
-                 unrealized_value: float,
-                 tags: list[str],
-                 notes: list[str]):
+                 average_cost: float):
         
         self.ticker = ticker
         self.shares = shares
         self.average_cost = average_cost
-        self.current_price = current_price
-        self.total_value = total_value
-        self.unrealized_value = unrealized_value
-        self.tags = tags
-        self.notes = notes
+
+    
+    def get_info(self, info_key: str) -> None:
+
+        ticker_info = yf.Ticker(self.ticker).info
+        if not ticker_info:
+            raise ValueError("PositionError: No ticker data.")
+        match info_key:
+            
+            case "current_price":
+                try:
+                    current_price = ticker_info["regularMarketPrice"]
+                except KeyError:
+                    raise ValueError("PositionError: Current Price Not Found.")
+                return current_price
+            
+            case "sector":
+                try:
+                    current_sector = ticker_info["sector"]
+                except KeyError:
+                    raise ValueError("PositionError: Sector Not Found.")
+                return current_sector
+            case _:
+                raise ValueError("PositionError: Invalid Info Key.")
+
+    def update(self, update_key: str, updated_value):
+        update_keys = {"shares", "average_cost"}
+        
+        if update_key not in update_keys:
+            raise ValueError("PositionError: Invalid Update Key.")
+        
+        match update_key:
+            case "shares":
+                try:
+                    new_shares = float(updated_value)
+                except ValueError:
+                    raise ValueError("PositionError: Share count type invalid.")
+
+                if new_shares < 0:
+                    raise ValueError("PositionError: Share count must be 0+.")
+        
+                self.shares = new_shares
+            
+            case "average_cost":
+                try:
+                    new_average_cost = float(updated_value)
+                except ValueError:
+                    raise ValueError("PositionError: Average Cost type invalid.")
+
+                if new_average_cost < 0:
+                    raise ValueError("PositionError: Average Cost must be 0+.")
+        
+                self.average_cost = new_average_cost
+
+
+
+
+
+    @property
+    def current_price(self):
+        return self.get_info("current_price")  
+    @property
+    def sector(self):
+        return self.get_info("sector")  
+    @property
+    def total_value(self):
+        return self.current_price * self.shares
+    @property
+    def unrealized_gain(self):
+        return (self.current_price - self.average_cost) * self.shares
+
+
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,16 @@
+import yfinance as yf
+from classes.Position import Position
+
 
 def main():
-    pass
+    
+    test_position = Position(ticker="msft", shares=1, average_cost=0)
+
+    print(test_position.unrealized_gain)
+    
+
+    
+    
 
 
 if __name__ == "__main__":
