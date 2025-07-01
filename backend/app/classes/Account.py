67ff441f274c73e492288b504375a4feb1fd6bdc
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-06-30 22:06:29 -0700

    Change iterable summing for total_value/unr_gain to instance var for O(1).

--- backend/app/classes/Account.py
+++ backend/app/classes/Account.py
@@ -6,43 +6,38 @@ class Account:
 
         self.name = name
         self.institution = institution
+        self._total_value = 0.0
+        self._unrealized_gain = 0.0
         self.positions: dict[str, Position] = {}
 
     def add_position(self, position: Position):
 
         self.positions[position.ticker] = position
+        self._total_value += position.total_value
+        self._unrealized_gain += position.unrealized_gain
         print(f"{position.shares} share(s) of {position.ticker.upper()} added to {self.name} ({self.institution.title()}) successfully.")
 
     def delete_position(self, position:Position=None, ticker:str=None):
         
         if position:
             rem = self.positions.pop(position.ticker, None)
-            if rem is None:
-                raise KeyError("AccountError: Position not found, removal failed.")
-            else:
-                print(f"{position.ticker.upper()} removed from {self.name.title()} ({self.institution.title()}) successfully.")
         elif ticker:
             rem = self.positions.pop(ticker.lower(), None)
-            if rem is None:
-                raise KeyError("AccountError: Ticker not found, removal failed.")
-            else:
-                print(f"{ticker.upper()} removed from {self.name.title()}, {self.institution.title()} successfully.")
         else:
             raise ValueError("AccountError: Must provide a Position or ticker.")
+        if rem is None:
+            raise KeyError(f"AccountError: {position.ticker if position else ticker} not found, removal failed.")
+        self._total_value -= rem.total_value
+        self._unrealized_gain -= rem.unrealized_gain
+        print(f"{ticker.upper()} removed from {self.name.title()}, {self.institution.title()} successfully.")
 
     @property
     def total_value(self):
-        if not self.positions:
-            return 0.0
-        return sum(position.total_value for position in self.positions.values())
-
-
+        return self._total_value
     
     @property
     def unrealized_gain(self):
-        if not self.positions:
-            return 0.0
-        return sum(position.unrealized_gain for position in self.positions.values())
+        return self._unrealized_gain
     
 
 
