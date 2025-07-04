dbaa075531132de32f2613985dfb04abea21ff1e
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-07-03 21:35:16 -0700

    Add customtkinter, renamed main -> testing, made main tkinter loop.

---
+++ app/testing.py
@@ -0,0 +1,36 @@
+import yfinance as yf
+from classes.Position import Position
+from classes.Account import Account
+from classes.Portfolio import Portfolio
+
+
+def main():
+
+    # Create Portfolio Object
+    test_portfolio = Portfolio(name="Deming Investments")
+
+    # Create Account Object(s)
+    test_account1 = Account(name="Individual Taxable", institution="Robinhood")
+    test_account2 = Account(name="Roth IRA", institution="Fidelity")
+
+    # Add Account(s) to Portfolio
+    test_portfolio.add_account(test_account1)
+    test_portfolio.add_account(test_account2)
+
+    # Create Position(s)
+    test_position1 = Position(ticker="aapl", shares=1, average_cost=213.55)
+    test_position2 = Position(ticker="aapl", shares=1, average_cost=213.55)
+
+    # Add Position(s) to Account(s)
+    test_account1.add_position(test_position1)
+    test_account2.add_position(test_position2)
+
+    
+    print(test_portfolio.stock_asset_value)
+    print(test_portfolio.cash)
+    print(test_portfolio.total_value)
+    print(test_portfolio.unrealized_gain)
+
+
+if __name__ == "__main__":
+    main()
