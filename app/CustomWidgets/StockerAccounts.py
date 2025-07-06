02072ef15a48f1622ef6af23eb5bcbb1d6899f1b
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-07-06 00:34:38 -0700

    Seperated Accounts page leftside comps.

--- app/CustomWidgets/StockerAccounts.py
+++ app/CustomWidgets/StockerAccounts.py
@@ -11,7 +11,7 @@ class StockerAccounts(ctk.CTkFrame):
 
         # Accounts Frame Left Section
         self.StockerAccountsLeftFrame = ctk.CTkFrame(
-            master=self, width=400, border_width=1
+            master=self, width=300, fg_color="transparent"
         )
         self.StockerAccountsLeftFrame.pack_propagate(False)
         self.StockerAccountsLeftFrame.grid_propagate(False)
@@ -19,41 +19,28 @@ class StockerAccounts(ctk.CTkFrame):
             side="left", fill="y", padx=(15, 7.5), pady=(5, 15)
         )
 
-        self.StockerAccountsLeftFrame.columnconfigure(0, weight=1)
-        self.StockerAccountsLeftFrame.columnconfigure(1, weight=1)
-
-        self.StockerAccountsLeftFrame.rowconfigure(0, weight=1)  # Accounts Title
-        self.StockerAccountsLeftFrame.rowconfigure(
-            1, weight=1
-        )  # View Account(s) Label + Accounts Drop Down
-        self.StockerAccountsLeftFrame.rowconfigure(
-            2, weight=1
-        )  # View Account(s) Submit Button
-        self.StockerAccountsLeftFrame.rowconfigure(
-            3, weight=1
-        )  # View Account(s) Submit Status Label
-        self.StockerAccountsLeftFrame.rowconfigure(4, weight=1)  # Blank Space
-        self.StockerAccountsLeftFrame.rowconfigure(5, weight=1)  # Positions Title
-        self.StockerAccountsLeftFrame.rowconfigure(
-            6, weight=1
-        )  # Search Position(s) Label + Textbox
-        self.StockerAccountsLeftFrame.rowconfigure(
-            7, weight=1
-        )  # Search Positions Submit Button
-        self.StockerAccountsLeftFrame.rowconfigure(
-            8, weight=1
-        )  # Search Positions Submit Status Label
-
         # <-------------------------- ACCOUNT SELECT DROPDOWN MENU -------------------------->
+        self.account_select_frame = ctk.CTkFrame(
+            master=self.StockerAccountsLeftFrame,
+        )
+        self.account_select_frame.pack(side="top", fill="x", pady=(75,0))
+        self.account_select_frame.grid_propagate(False)
+        self.account_select_frame.rowconfigure(0, weight=1)
+        self.account_select_frame.rowconfigure(1, weight=1)
+        self.account_select_frame.rowconfigure(2, weight=1)
+        self.account_select_frame.rowconfigure(3, weight=1)
+
+        self.account_select_frame.columnconfigure(0, weight=1)
+        self.account_select_frame.columnconfigure(1, weight=1)
 
         # TITLE: "Accounts"
         self.accounts_left_frame_title_label = ctk.CTkLabel(
-            self.StockerAccountsLeftFrame, text=f"Accounts", fg_color="transparent"
+            self.account_select_frame, text=f"Accounts", fg_color="transparent"
         ).grid(row=0, column=0, columnspan=2)
 
         # ACCOUNT SELECT DROPDOWN | DROPDOWN LABEL: "Select Account(s)"
         self.select_accounts_dropdown_dropdown_label = ctk.CTkLabel(
-            self.StockerAccountsLeftFrame,
+            self.account_select_frame,
             text=f"Select Account(s)",
             fg_color="transparent",
         ).grid(row=1, column=0)
@@ -61,65 +48,75 @@ class StockerAccounts(ctk.CTkFrame):
         # ACCOUNT SELECT DROPDOWN | DROPDOWN-BOX: Account Names
         self.account_names = ["Roth IRA", "Taxable Account", "Traditional 401k", "all"]
         self.select_accounts_dropdown_dropdown = ctk.CTkComboBox(
-            master=self.StockerAccountsLeftFrame, values=self.account_names
+            master=self.account_select_frame, values=self.account_names
         ).grid(row=1, column=1)
 
         # ACCOUNT SELECT DROPDOWN | SUBMIT BUTTON: "View Account(s)"
         self.submit_account_view_button = ctk.CTkButton(
-            master=self.StockerAccountsLeftFrame, text="View Account(s)"
+            master=self.account_select_frame, text="View Account(s)"
         ).grid(row=2, column=0, columnspan=2)
 
         # ACCOUNT SELECT DROPDOWN | STATUS LABEL: "Select Account(s)"
         self.account_dropdown_status_message = "N/A"
         self.select_accounts_dropdown_status_label = ctk.CTkLabel(
-            self.StockerAccountsLeftFrame,
+            self.account_select_frame,
             text=f"Status: {self.account_dropdown_status_message}",
             fg_color="transparent",
         ).grid(row=3, column=0, columnspan=2)
 
         # <-------------------------- POSITION SEARCH MENU -------------------------->
+        self.position_search_frame = ctk.CTkFrame(
+            master=self.StockerAccountsLeftFrame,
+        )
+        self.position_search_frame.pack(side="bottom", fill="x", pady=(0,75))
+        self.position_search_frame.grid_propagate(False)
+        self.position_search_frame.rowconfigure(0, weight=1)
+        self.position_search_frame.rowconfigure(1, weight=1)
+        self.position_search_frame.rowconfigure(2, weight=1)
+        self.position_search_frame.rowconfigure(3, weight=1)
 
+        self.position_search_frame.columnconfigure(0, weight=1)
+        self.position_search_frame.columnconfigure(1, weight=1)
         # POSITION SEARCH MENU | TITLE: "Positions"
         self.positions_search_menu_title_label = ctk.CTkLabel(
-            self.StockerAccountsLeftFrame, text=f"Positions", fg_color="transparent"
-        ).grid(row=5, column=0, columnspan=2)
+            self.position_search_frame, text=f"Positions", fg_color="transparent"
+        ).grid(row=0, column=0, columnspan=2)
 
         # POSITION SEARCH MENU | POSITION SEARCHBOX LABEL: "Search Position(s)"
         self.positions_search_searchbox_label = ctk.CTkLabel(
-            self.StockerAccountsLeftFrame,
+            self.position_search_frame,
             text=f"Search Position(s)",
             fg_color="transparent",
-        ).grid(row=6, column=0)
+        ).grid(row=1, column=0)
 
         # POSITION SEARCH MENU | POSITION SEARCHBOX: "Ticker"
         self.positions_search_searchbox = ctk.CTkEntry(
-            master=self.StockerAccountsLeftFrame, placeholder_text="Ticker (AAPL)"
-        ).grid(row=6, column=1)
+            master=self.position_search_frame, placeholder_text="Ticker (AAPL)"
+        ).grid(row=1, column=1)
 
         # POSITION SEARCH MENU | POSITION SEARCH SUBMIT BUTTON: "Search Position(s)"
         self.positions_search_submit_button = ctk.CTkButton(
-            master=self.StockerAccountsLeftFrame, text="Search Position(s)"
-        ).grid(row=7, column=0, columnspan=2)
+            master=self.position_search_frame, text="Search Position(s)"
+        ).grid(row=2, column=0, columnspan=2)
 
         # POSITION SEARCH MENU | POSITION SEARCH SUBMIT STATUS LABEL: "Status: ..."
         self.positions_search_status_message = "N/A"
         self.position_search_status_label = ctk.CTkLabel(
-            self.StockerAccountsLeftFrame,
+            self.position_search_frame,
             text=f"Position Search Status: {self.positions_search_status_message}",
             fg_color="transparent",
-        ).grid(row=8, column=0, columnspan=2)
-
-        # Accounts Frame Right Section
-        self.StockerAccountsRightFrame = ctk.CTkFrame(
-            master=self, width=710, fg_color="transparent"
-        )
-        self.StockerAccountsRightFrame.pack_propagate(False)
-        self.StockerAccountsRightFrame.pack(
-            side="right", fill="y", padx=(7.5, 15), pady=(5, 15)
-        )
+        ).grid(row=3, column=0, columnspan=2)
 
         # <-------------------------- SELECETED ACCOUNT INFORMATION HEADER -------------------------->
 
+        # Accounts Frame Right Section
+        self.StockerAccountsRightFrame = ctk.CTkFrame(
+            master=self, fg_color="transparent"
+        )
+        self.StockerAccountsRightFrame.pack_propagate(False)
+        self.StockerAccountsRightFrame.pack(
+            side="right", fill="both", expand=True, padx=(7.5, 15), pady=(5, 15)
+        )
         self.selected_account_information_header = ctk.CTkFrame(
             master=self.StockerAccountsRightFrame, height=75, fg_color="transparent"
         )
