7938ce0ea3d4cba2ddd30a127d2082dbec8fbb61
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-07-05 21:51:53 -0700

    Add Account page header for selected Accont.

--- app/CustomWidgets/StockerAccounts.py
+++ app/CustomWidgets/StockerAccounts.py
@@ -39,7 +39,7 @@ class StockerAccounts(ctk.CTkFrame):
         self.select_accounts_dropdown_dropdown_label = ctk.CTkLabel(self.StockerAccountsLeftFrame, text=f"Select Account(s)", fg_color="transparent").grid(row=1,column=0)
         
         # ACCOUNT SELECT DROPDOWN | DROPDOWN-BOX: Account Names
-        self.account_names = ["Roth IRA", "Taxable Account", "Traditional 401k"]
+        self.account_names = ["Roth IRA", "Taxable Account", "Traditional 401k", "all"]
         self.select_accounts_dropdown_dropdown = ctk.CTkComboBox(master=self.StockerAccountsLeftFrame, values=self.account_names).grid(row=1,column=1)
 
         # ACCOUNT SELECT DROPDOWN | SUBMIT BUTTON: "View Account(s)"
@@ -72,5 +72,57 @@ class StockerAccounts(ctk.CTkFrame):
         
         # Accounts Frame Right Section
         self.StockerAccountsRightFrame = ctk.CTkFrame(master=self, width=710, border_color="black", border_width=1)
+        self.StockerAccountsRightFrame.pack_propagate(False)
+        self.StockerAccountsRightFrame .pack(side="right", fill="y", padx=(7.5, 15), pady=(5, 15))
 
-        self.StockerAccountsRightFrame.pack(side="right", fill="y", padx=(7.5, 15), pady=(5, 15))
+
+
+
+
+
+
+        # <-------------------------- SELECETED ACCOUNT INFORMATION HEADER -------------------------->
+
+        self.selected_account_information_header = ctk.CTkFrame(master=self.StockerAccountsRightFrame, height=75, border_color="black", border_width=1)
+        self.selected_account_information_header.pack_propagate(False)
+        self.selected_account_information_header.pack(fill="x")
+        self.selected_account_information_header.grid_propagate(False)
+
+
+        self.selected_account_information_header.rowconfigure(0, weight=1)
+        self.selected_account_information_header.rowconfigure(1, weight=1)
+        self.selected_account_information_header.columnconfigure(0, weight=1)
+        self.selected_account_information_header.columnconfigure(1, weight=1)
+        self.selected_account_information_header.columnconfigure(2, weight=1)
+
+        # SELECETED ACCOUNT INFORMATION HEADER | SELECTED ACCOUNT LABEL: "Selected Account: ..."
+        self.selected_account_name = "Roth IRA"
+        self.selected_account_label = ctk.CTkLabel(self.selected_account_information_header, text=f"Selected Account(s): {self.selected_account_name}", fg_color="transparent").grid(row=0, column=0, sticky="w", padx=15,)
+
+        # SELECETED ACCOUNT INFORMATION HEADER | SELECTED ACCOUNT TOTAL VALUE LABEL: "Total Value: ..."
+        self.selected_account_total_value = 1500.0
+        self.selected_account_total_value_label = ctk.CTkLabel(self.selected_account_information_header, text=f"Total Value: ${self.selected_account_total_value}", fg_color="transparent").grid(row=1, column=0, padx=15, sticky="w")
+        
+        # SELECETED ACCOUNT INFORMATION HEADER | SELECTED ACCOUNT CASH RESERVE LABEL: "Cash Reserve"
+        self.selected_account_cash = 500.00
+        self.selected_account_cash_label = ctk.CTkLabel(self.selected_account_information_header, text=f"Cash Reserve: ${self.selected_account_cash}", fg_color="transparent").grid(row=0, column=1)
+        
+        # SELECETED ACCOUNT INFORMATION HEADER | SELECTED ACCOUNT UPDATE CASH BUTTON: "Update Cash"
+        self.selected_account_update_cash_button = ctk.CTkButton(self.selected_account_information_header, text=f"Update Cash").grid(row=1, column=1)
+
+        # SELECETED ACCOUNT INFORMATION HEADER | SELECTED ACCOUNT STOCK ASSET VALUE LABEL: "Stock Asset Value: ..."
+        self.selected_account_stock_asset_value = 1000.0
+        self.selected_account_stock_asset_value_label = ctk.CTkLabel(self.selected_account_information_header, text=f"Stock Asset Value: ${self.selected_account_stock_asset_value}", fg_color="transparent").grid(row=0, column=2)
+        
+        # SELECETED ACCOUNT INFORMATION HEADER | SELECTED ACCOUNT STOCK ASSET VALUE BUTTON: "Refresh Value"
+        self.selected_account_refresh_value_button = ctk.CTkButton(self.selected_account_information_header, text=f"Refresh Value").grid(row=1, column=2)
+
+
+        
+
+
+
+
+
+
+        
